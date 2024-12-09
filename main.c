088e8aa46bd2e1319218be26bfe6ed815ecbdc08
Gabriel9975 <julien.lopes6987@gmail.com>
2024-12-09 15:09:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_acodec.h>
+#include <allegro4/allegro.h>
+#include <allegro4/allegro_image.h>
+#include <allegro4/allegro_primitives.h>
+#include <allegro4/allegro_audio.h>
+#include <allegro4/allegro_acodec.h>
 #include "game.h"
 #include "graphics.h"
 #include "input.h"
